e83934b02b9c484c60491ed2b9ed3c0a9ef3b2b7
Blake Sadoway <blakesadoway@gmail.com>
2017-12-19 21:25:49 +0000

    Refactored into separate connection file

---
+++ db.js
@@ -0,0 +1,21 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+
+const config = {
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+};
+
+module.exports = {
+  connect: (done /* our callback */) => {
+    const client = new pg.Client(config);
+    client.connect((error) => {
+      done(error, client);
+    });
+  }
+}
--- lookup_people.js
+++ lookup_people.js
@@ -1,37 +1,24 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
-var dateFormat = require('dateformat');
-
-
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
+const dateFormat = require('dateformat');
+const db = require('./db');
 
 const arg = process.argv[2];
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [arg], (err, result) => {
-    if (err) {
-      return console.error("error running query", err);
-    }
-    printRecord(result);
-    client.end();
+
+function searchUsersByName(name) {
+  db.connect((error, client) => {
+    client.query("SELECT * FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [name], (err, result) => {
+      printRecord(result);
+      client.end();
+    });
   });
-});
+}
 
-
-function printRecord(result){
+function printRecord(result) {
   const formattedDate = dateFormat(result.rows[0].birthdate, "fullDate");
 
   console.log("Searching ...");
   console.log(`Found ${result.rowCount} person(s) by the name ${arg}:`);
   console.log(`${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born ${formattedDate}`); //output: 1
 }
+
+searchUsersByName(arg);
