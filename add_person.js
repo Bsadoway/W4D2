b7b8341e7b5de2ee3a38d34e54e7a54e0e547212
Blake Sadoway <blakesadoway@gmail.com>
2017-12-19 22:37:16 +0000

    added Knex

---
+++ add_person.js
@@ -0,0 +1,26 @@
+const settings = require("./settings"); // settings.json
+
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: settings.hostname,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
+  }
+});
+
+const first_name = process.argv[2];
+const last_name = process.argv[3];
+const birthdate = process.argv[4];
+
+knex.insert({
+    first_name: first_name,
+    last_name: last_name,
+    birthdate: birthdate
+  })
+  .into("famous_people").asCallback((error, results) => {
+    knex.destroy();
+  });
+// handle error
---
+++ knex_lookup.js
@@ -0,0 +1,30 @@
+const dateFormat = require('dateformat');
+const settings = require("./settings"); // settings.json
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+const arg = process.argv[2];
+
+knex.select().from("famous_people").where({ last_name: arg }).asCallback((error, results) => {
+  // handle error
+  results.forEach((result) => {
+    printRecord(result);
+  });
+  knex.destroy();
+});
+
+
+function printRecord(result) {
+  const formattedDate = dateFormat(result.birthdate, "fullDate");
+  console.log("Searching ...");
+  console.log(`Found 1 person(s) by the name:`);
+  console.log(`${result.id}: ${result.first_name} ${result.last_name}, born ${formattedDate}`); //output: 1
+}
